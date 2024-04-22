7552c7fe12ec846d5c150b447358106a95f66109
Lulekoh <lulekoleoquintus12@gmail.com>
2024-04-22 21:19:00 +0000

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,8 +2,8 @@ export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
   for (const idx of array) {
     const value = idx;
-    array.push(appendString + value);
+    arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
